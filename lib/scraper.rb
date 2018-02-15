4d8769a2e364ffb335a07a979fb582d8dbd57b88
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 12:47:45 -0500

    Scraper parses headlines

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,12 @@
 class Scraper
-  def self.scrape_index_page(_index_url)
+  def self.scrape_index_page
     html = open('https://www.avclub.com/c/newswire')
     doc = Nokogiri::HTML(html)
+
+    main = doc.css('section.main .main__content .storytype__content article')
+
+    main.each do |element|
+      puts element.css('.headline.entry-title a').text
+    end
   end
 end
