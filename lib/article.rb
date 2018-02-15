18972aa58d89f223ae3bc789f4f756450cb3e543
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 15:08:26 -0500

    Removed all class variable, changed display_all to display

--- lib/article.rb
+++ lib/article.rb
@@ -2,29 +2,19 @@
 class Article
   attr_accessor :name, :summary, :url, :full_story, :date
 
-  @@all = []
-
   def initialize(name, summary, url, date)
     self.name = name
     self.summary = summary
     self.url = url
     self.date = date
-
-    self.class.all << self
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.display_all
-    all.each do |article|
-      y, m, d, h, min, s = article.date
-      puts article.name
-      puts "#{y} #{m} #{d}"
-      puts article.full_story ? article.full_story : article.summary
-      puts '---'
-      puts
-    end
+  def display
+    y, m, d, h, min, s = date
+    puts name
+    puts "#{y} #{m} #{d}"
+    puts full_story ? full_story : summary
+    puts '---'
+    puts
   end
 end
