25ca3d5fde5dbe5bda570b24e331feb99317834c
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 13:06:50 -0500

    Scraper grabs article name, summary, and url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+# Scrapes AV Club newswire page
 class Scraper
   def self.scrape_index_page
     html = open('https://www.avclub.com/c/newswire')
@@ -6,7 +7,13 @@ class Scraper
     main = doc.css('section.main .main__content .storytype__content article')
 
     main.each do |element|
-      puts element.css('.headline.entry-title a').text
+      title = element.css('.headline.entry-title a')
+      name = title.text
+      url = title.attribute('href').value
+
+      summary = element.css('.excerpt.entry-summary p')
+
+      Article.new(name, summary, url)
     end
   end
 end
