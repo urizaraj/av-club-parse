48b79f2cc31ddf2e1c76f5dd5e1a34498f0d2628
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 17:41:22 -0500

    Tag / Article relationship

--- lib/article.rb
+++ lib/article.rb
@@ -1,12 +1,14 @@
 # Article name, summary, and URL
 class Article
-  attr_accessor :name, :summary, :url, :full_story, :date
+  attr_accessor :name, :summary, :url, :full_story, :date, :tags
 
-  def initialize(name, summary, url, date)
+  def initialize(name, summary, url, date, tag_names)
     self.name = name.delete("\n")
     self.summary = summary
     self.url = url
     self.date = date
+    self.tags = []
+    add_tags(tag_names)
   end
 
   def display
@@ -16,5 +18,12 @@ class Article
     puts full_story ? full_story : summary
     puts '---'
     puts
+  end
+
+  def add_tags(tag_names)
+    tag_names.each do |tag_name, tag_url|
+      tag = Tag.new(tag_name, tag_url, self)
+      tags << tag
+    end
   end
 end
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,6 +1,9 @@
+# Class for Article tags
 class Tag
-  attr_accessor :name
-  def initialize(name)
+  attr_accessor :name, :url, :articles
+  def initialize(name, url, article)
     self.name = name
+    self.url = url
+    self.articles = [article]
   end
 end
