ce65ea88e953d9b9b7a7a4289dc254f4450194a9
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 14:17:20 -0500

    Added get_date_array method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,10 @@
 # Scrapes AV Club newswire page
 class Scraper
+  def self.fetch_doc(url)
+    html = open(url)
+    Nokogiri::HTML(html)
+  end
+
   def self.scrape_index_page
     doc = fetch_doc('https://www.avclub.com/c/newswire')
 
@@ -10,15 +15,17 @@ class Scraper
       name = title.text
       url = title.attribute('href').value
 
+      date = get_date_array
       summary = element.css('.excerpt.entry-summary p').text
 
-      Article.new(name, summary, url)
+      Article.new(name, summary, url, date)
     end
   end
 
-  def self.fetch_doc(url)
-    html = open(url)
-    Nokogiri::HTML(html)
+  def self.get_date_array(element)
+    time = element.css('time').attribute('datetime').value
+    date = time.scan(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/)
+    date.flatten
   end
 
   def self.scrape_article(url)
