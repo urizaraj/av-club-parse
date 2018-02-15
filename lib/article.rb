22055644168bbeadac7fece5cc391e8a7f4f90ab
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 14:17:43 -0500

    Added date instance variable

--- lib/article.rb
+++ lib/article.rb
@@ -1,13 +1,14 @@
 # Article name, summary, and URL
 class Article
-  attr_accessor :name, :summary, :url, :full_story
+  attr_accessor :name, :summary, :url, :full_story, :date
 
   @@all = []
 
-  def initialize(name, summary, url)
+  def initialize(name, summary, url, date)
     self.name = name
     self.summary = summary
     self.url = url
+    self.date = date
 
     self.class.all << self
   end
@@ -18,7 +19,9 @@ class Article
 
   def self.display_all
     all.each do |article|
+      y, m, d, h, min, s = article.date
       puts article.name
+      puts "#{y} #{m} #{d}"
       puts article.full_story ? article.full_story : article.summary
       puts '---'
       puts
