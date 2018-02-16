aefed6131e09719e6d033ab9000e529d5ab2bcfe
Alexander Urizar <urizaraj@gmail.com>
2018-02-16 15:31:31 -0500

    Added size attribute

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,14 +1,16 @@
 # Class for Article tags
 class Tag
-  attr_accessor :name, :url, :articles
+  attr_accessor :name, :url, :articles, :size
 
   def initialize(name, url)
     self.name = name
     self.url = url
     self.articles = []
+    self.size = 0
   end
 
   def add_article(article)
     articles << article
+    self.size += 1
   end
 end
