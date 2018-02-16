fb586891ffd5cfdd84fa27a0cd6b404c0f7ae492
Alexander Urizar <urizaraj@gmail.com>
2018-02-16 11:16:10 -0500

    Added comments

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,20 +6,25 @@ class Scraper
   end
 
   def self.scrape_index_page(start_time = 0)
+    # get the URL, based on whether a start time was provided
     url = 'https://www.avclub.com/c/newswire'
     url << "?startTime=#{start_time}" unless start_time.zero?
     doc = fetch_doc(url)
 
+    # retrieve all 'article' HTML elements
     main = doc.css('section.main .main__content .storytype__content article')
 
+    # scrape each article element
     main.map { |element| scrape_index_element(element) }
   end
 
   def self.scrape_index_element(element)
+    # get the title element and extract the name and url of the article
     title = element.css('.headline.entry-title a')
     name = title.text
     url = title.attribute('href').value
 
+    # get the date, summary, and tags
     date = get_date_array(element)
     summary = element.css('.excerpt.entry-summary p').text
     tags = get_tag_array(element)
