96d8aafbec019c8dcf58d06a398a6cbafc817eaa
Alexander Urizar <urizaraj@gmail.com>
2018-02-16 11:17:51 -0500

    Created add_article method

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,10 +1,14 @@
 # Class for Article tags
 class Tag
   attr_accessor :name, :url, :articles
-  
-  def initialize(name, url, article)
+
+  def initialize(name, url)
     self.name = name
     self.url = url
-    self.articles = [article]
+    self.articles = []
+  end
+
+  def add_article(article)
+    articles << article
   end
 end
