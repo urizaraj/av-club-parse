6833406c843436268d63a48484eea6b7279efb6d
Alexander Urizar <urizaraj@gmail.com>
2018-02-22 12:05:47 -0500

    Broke commands into methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -19,48 +19,76 @@ class AVParseCommandLine
       input = gets.strip.split
       puts
       if input[0] == 'titles'
-        avparser.display_titles
+        titles
 
       elsif input[0] == 'article'
-        valid = avparser.display_article(input[1].to_i)
-        puts "Invalid article\n\n" unless valid
+        article(input[1].to_i)
 
       elsif input[0] == 'more'
-        print 'loading more articles... '
-        avparser.more_articles
-        print 'done!'
-        puts
-        puts
+        more
 
       elsif input[0] == 'tags'
-        avparser.display_tags
+        tags
 
       elsif input[0] == 'all'
-        print 'loading all articles... '
-        avparser.full_story_all
-        print 'done!'
-        puts
-        puts
+        all
 
       elsif input[0] == 'tag'
-        valid = avparser.display_single_tag(input[1])
-        puts "Invalid tag\n\n" unless valid
+        tag(input[1])
 
       elsif input[0] == 'help'
-        puts 'Commands: '
-        puts 'titles'
-        puts 'article [number]'
-        puts 'more'
-        puts 'tags'
-        puts 'all'
-        puts 'tag [tag name]'
-        puts 'quit'
-        puts 'help'
-        puts
+        help
 
       elsif input[0] == 'quit'
         break
       end
     end
+  end
+
+  def titles
+    avparser.display_titles
+  end
+
+  def article(i)
+    valid = avparser.display_article(i)
+    puts "Invalid article\n\n" unless valid
+  end
+
+  def more
+    print 'loading more articles... '
+    avparser.more_articles
+    print 'done!'
+    puts
+    puts
+  end
+
+  def tags
+    avparser.display_tags
+  end
+
+  def all
+    print 'loading all articles... '
+    avparser.full_story_all
+    print 'done!'
+    puts
+    puts
+  end
+
+  def tag(i)
+    valid = avparser.display_single_tag(i)
+    puts "Invalid tag\n\n" unless valid
+  end
+
+  def help
+    puts 'Commands: '
+    puts 'titles'
+    puts 'article [number]'
+    puts 'more'
+    puts 'all'
+    puts 'tags'
+    puts 'tag [tag name]'
+    puts 'quit'
+    puts 'help'
+    puts
   end
 end
